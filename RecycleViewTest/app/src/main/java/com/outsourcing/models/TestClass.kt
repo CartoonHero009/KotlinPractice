ec8cd7e245b0141b5a2c06ac5c3939f7e4a359a5
Unknown <rockbandtech@gmail.com>
2019-10-02 00:57:37 +0800

    Add custom toast

--- RecycleViewTest/app/src/main/java/com/outsourcing/models/TestClass.kt
+++ RecycleViewTest/app/src/main/java/com/outsourcing/models/TestClass.kt
@@ -1,7 +1,13 @@
 package com.outsourcing.models
 
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.outsourcing.recycleviewtest.R
 
 
 class TestClass {
@@ -17,4 +23,18 @@ class TestClass {
         }
         return uid
     }
+
+//    fun Toast.createToast(context: Context, message:String, gravity:Int, duration:Int){
+//        val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//        /*first parameter is the layout you made
+//        second parameter is the root view in that xml
+//         */
+//        val layout = inflater.inflate(R.layout.custom_toast_layout, (context as Activity).findViewById<ViewGroup>(R.id.custom_toast_container))
+//
+//        layout.findViewById<TextView>(R.id.text).text = message
+//        setGravity(gravity, 0, 0)
+//        setDuration(Toast.LENGTH_LONG);
+//        setView(layout);
+//        show()
+//    }
 }
